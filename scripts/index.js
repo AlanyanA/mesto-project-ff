969e6c446d6539a25ff646cdb790f7a8e90a6eac
Arman Alanyan <armanalanyan007@gmail.com>
2025-02-11 19:22:20 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
+const currentCards = document.querySelector('.places__list');
+const placesList = document.querySelector('.places__list');
+function makeCard(card) {
+    const cloneCard = cardTemplate.cloneNode(true);
+    cloneCard.querySelector('.card__image').alt = card.name;
+    cloneCard.querySelector('.card__image').src = card.link;
+    cloneCard.querySelector('.card__title').textContent = card.name;
+    const deleteButton = cloneCard.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => deleteCard(cloneCard));
+    return cloneCard;
+}
 
-// @todo: DOM узлы
+function deleteCard(item) {
+    item.remove();
+}
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+for(let i = 0; i < initialCards.length; i++){
+    placesList.append(makeCard(initialCards[i]));
+}
